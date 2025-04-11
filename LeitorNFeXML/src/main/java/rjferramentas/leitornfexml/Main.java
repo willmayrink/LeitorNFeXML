09acb7fa315fe547bb24bec64e5aa5f257561e56
Will Mayrink <will.mayrink@gmail.com>
2025-04-11 19:52:49 -0300

    Fixed Look'n Feel
    close#1
    
    We'll be using DarculaLaf; so I've got the `pom.xml` file configured with the Maven repository tags:
    
    ```xml
    <dependencies>
            <dependency>
                <groupId>com.formdev</groupId>
                <artifactId>flatlaf</artifactId>
                <version>3.5.4</version>
            </dependency>
        </dependencies>
    ```
    
    Right after the dependency insertion; I made it, so the method `initComponents();` would first call for the LaF.
    
    ```java
     FlatDarculaLaf.setup()
    ```

--- LeitorNFeXML/src/main/java/rjferramentas/leitornfexml/Main.java
+++ LeitorNFeXML/src/main/java/rjferramentas/leitornfexml/Main.java
@@ -4,6 +4,9 @@
  */
 package rjferramentas.leitornfexml;
 
+import com.formdev.flatlaf.FlatDarculaLaf;
+
+
 /**
  *
  * @author Will
@@ -73,22 +76,7 @@ public class Main extends javax.swing.JFrame {
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
+        FlatDarculaLaf.setup();
         //</editor-fold>
 
         /* Create and display the form */
